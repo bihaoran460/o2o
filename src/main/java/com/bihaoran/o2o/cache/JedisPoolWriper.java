bd3eb8b9bdb7445cef43b06ec2e071d539c32c06
bihaoran <bihaoran@LAPTOP-NUFRR7N2>
2019-07-09 22:08:09 +0800

    增加redis缓存

---
+++ src/main/java/com/bihaoran/o2o/cache/JedisPoolWriper.java
@@ -0,0 +1,26 @@
+package com.bihaoran.o2o.cache;
+
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+public class JedisPoolWriper {
+	private JedisPool jedisPool;
+
+	public JedisPoolWriper(final JedisPoolConfig poolConfig, final String host,
+			final int port) {
+		try {
+			jedisPool = new JedisPool(poolConfig, host, port);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public JedisPool getJedisPool() {
+		return jedisPool;
+	}
+
+	public void setJedisPool(JedisPool jedisPool) {
+		this.jedisPool = jedisPool;
+	}
+
+}
